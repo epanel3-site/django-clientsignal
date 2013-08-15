86f3dfaea7a5f7a06ccfe4b286d072f6ba8d4e72
Will Barton <will@willbarton.com>
2013-08-15 07:00:58 -0400

    Add license to source files

--- clientsignal/redisconn.py
+++ clientsignal/redisconn.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright 2012 Will Barton. 
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions
+# are met:
+# 
+#   1. Redistributions of source code must retain the above copyright 
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright 
+#      notice, this list of conditions and the following disclaimer in the 
+#      documentation and/or other materials provided with the distribution.
+#   3. The name of the author may not be used to endorse or promote products
+#      derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+# AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
+# THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+# ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
 import tornado
 
@@ -22,7 +49,6 @@ REDIS_URL = get_backend_url_parts(app_settings.CLIENTSIGNAL_BACKEND)
 REDIS = tornadoredis.Client(
                 host=REDIS_URL.get('host', 'localhost'),
                 port=REDIS_URL.get('port', 6379) or 6379)
-REDIS.connect()
 
 class RedisSignalConnection(BaseSignalConnection):
     # The redis channel is this signalconnection's endpoint, therefore
@@ -66,6 +92,7 @@ class RedisSignalConnection(BaseSignalConnection):
 
                     json_evt = json.dumps({'name':name, 'args':kwargs}, cls=get_class_or_func(app_settings.CLIENTSIGNAL_DEFAULT_ENCODER))
                     log.debug("BROADCAST: Encoding and Sending %s(%s) signal to Redis channel %s" % (name, json_evt, cls.__channel__))
+
                     try:
                         REDIS.publish(cls.__channel__, "%s:%s" % (name, json_evt))
                     except Exception, e:
